68caa6a3affb9e17a678cd45be54e61724d7bbea
xoo <xoo@outlook.it>
2023-10-23 01:19:01 +0800

    10/23

--- xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/config/PayNotifyConfig.java
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/config/PayNotifyConfig.java
@@ -1,15 +1,8 @@
 package com.xuecheng.learning.config;
 
-import com.alibaba.fastjson.JSON;
-import com.xuecheng.messagesdk.model.po.MqMessage;
-import com.xuecheng.messagesdk.service.MqMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
